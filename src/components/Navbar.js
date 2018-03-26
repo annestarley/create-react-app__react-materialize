2726cc23a59175faca18ec7b2d19c35dc2a6b37f
Anne Starley <annestarley@gmail.com>
2018-03-26 16:04:41 -0700

    first navbar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Modal, Row, Col, Button, Carousel} from 'react-materialize'
+
+const Navbar = () => {
+  return (
+    <div className='App'>
+      <nav>
+        <div className="App-header">
+          <Row>
+            <Col l={8}>
+              <h1>Paper <span className="title">Trader</span></h1>
+            </Col>
+            <Col l={4}>
+            <p>
+              <span>
+                <Modal
+                  header='Log in'
+                  trigger={<a>log in </a>}>
+                  <p>Login stuff</p>
+                </Modal>
+              </span>
+              or
+              <span>
+                <Modal
+                  header='Sign up'
+                  trigger={<a> sign up</a>}>
+                  <p>sign up stuff</p>
+                </Modal>
+              </span>
+            </p>
+          </Col>
+        </Row>
+      </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
